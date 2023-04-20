6ed73d3ee589c6b171d10aa0b346db15d5a7eac5
Sara Aliaga <sarialiaga5@gmail.com>
2023-04-20 16:23:00 +0000

    Completed Code Challenge: Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+
+        int sum = 0;
+        for (int value : nums) {
+           sum =  sum += value;   
+        }
+        return (double) sum/nums.length;
     }
+    
 }
